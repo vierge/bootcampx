346105e5f1c541572397a20ef6e6f18db300ce84
vierge <marancholia@gmail.com>
2020-02-06 18:54:45 +0000

    paramaterized node-postgres queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 const args = process.argv.slice(2);
 
+
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -8,13 +10,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT students.id, students.name, cohorts.name as cohort_name
-  FROM students
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${args[0]}%'
-  LIMIT ${args[1]};
-`)
+const queryString = `
+SELECT students.id, students.name, cohorts.name as cohort_name
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+const cohortName =  args[0];
+const limit = args[1] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort!`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher ASC;
-`)
+`
+const cohortName = args[0];
+
+const values = [`${cohortName}`];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teachers => {
     console.log(`${teachers.cohort}: ${teachers.teacher}`);
